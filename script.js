2f157c6e57ceae6662f6da9aeb2b5935dc15fa66
Ferdelicious <ashinofre@gmail.com>
2025-12-07 01:17:52 +0800

    responsive

--- script.js
+++ script.js
@@ -8,3 +8,33 @@ document.getElementById("contactBtn").addEventListener("click", function(e) {
     e.preventDefault();  
     this.classList.toggle("active");
 });
+
+
+/*responsive?*/
+const hamburger = document.getElementById("main-menu");
+const navBar = document.querySelector(".nav-bar");
+
+hamburger.addEventListener("click", () => {
+  navBar.classList.toggle("active");
+});
+
+
+document.addEventListener('click', function(e) {
+    if (window.innerWidth <= 1125) {
+        if (navBar.classList.contains('active') && 
+            !navBar.contains(e.target) && 
+            e.target !== hamburger && 
+            !hamburger.contains(e.target)) {
+            navBar.classList.remove('active');
+        }
+    }
+});
+
+
+document.querySelectorAll('.nav-btn:not(#contactBtn)').forEach(button => {
+    button.addEventListener('click', function() {
+        if (window.innerWidth <= 1125 && navBar.classList.contains('active')) {
+            navBar.classList.remove('active');
+        }
+    });
+});
